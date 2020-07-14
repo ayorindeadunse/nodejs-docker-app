205b8ed7fcb2e54c8456759b8087f82ec6139311
Ayorinde Adunse <adunseayorinde@gmail.com>
2020-07-14 13:43:12 +0100

    Added new docker node project

---
+++ src/User.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  hobbies: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
