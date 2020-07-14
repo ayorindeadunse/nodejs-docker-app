17924cf2d878ffdc651e3b4d17454a48e81dbce0
Ayorinde Adunse <adunseayorinde@gmail.com>
2020-07-14 21:54:24 +0100

    updated server.js

--- src/connection.js
+++ src/connection.js
@@ -2,3 +2,7 @@ const mongoose = require("mongoose");
 const User = require("./User.model");
 const connection =
   "mongodb+srv://ayorinde:aKYdsnocRG28VZdT@cluster0-x9wzf.mongodb.net/nodedocker?retryWrites=true&w=majority";
+const connectDatabase = () => {
+  return mongoose.connect(connection);
+};
+module.exports = connectDatabase;
